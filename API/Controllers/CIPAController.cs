b0835cf90a39236c15194597349ca56e5a9cee6e
Yewo Theu <yewotheu123456789@gmail.com>
2022-02-03 17:34:23 +0200

    Webscrapper for CIPA
    Created Webscrapper for CIPA
    
    For determining is client is a registered company based on UIN

---
+++ API/Controllers/CIPAController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using API.DTO;
+using OpenQA.Selenium.Firefox;
+
+namespace API.Controllers
+{
+    public class CIPAController : BaseApiController
+    {
+        public string fullUrl = "https://www.cipa.co.bw//ng-cipa-master/ui/create/entitySearch";
+        
+        [HttpGet("check/{uin}")]
+        public async Task<ActionResult<CompanySearchDto>> IncorporationCheck(string uin)
+        {
+            List<string> programmerLinks = new List<string>();
+
+            var options = new ChromeOptions()
+            {
+                BinaryLocation = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+            };
+
+            options.AddArguments(new List<string>() { "headless", "disable-gpu" });
+            var chromeDriver = new ChromeDriver(options);
+            chromeDriver.Navigate().GoToUrl(fullUrl);
+
+
+            //Search for search bar and search button and perform actions
+            IWebElement searchBar= chromeDriver.FindElement(By.TagName("input"));
+
+            searchBar.Clear();
+            searchBar.SendKeys(uin);
+
+            int count = 0;
+            var elements = chromeDriver.FindElements(By.TagName("a"));
+            foreach (var item in elements)
+            {
+                if (item.Text == "Search")
+                {
+                    count++;
+
+                    if (count == 2)
+                        item.Click();
+                }
+            }
+
+            await Task.Delay(10000);
+
+            var links = chromeDriver.FindElements(By.TagName("a"));
+
+            //Await response and capture info
+
+            string companyName = "";
+            count = 0;
+
+            foreach (var item in links)
+            {
+                count++;
+
+                if (count == 37)
+                    companyName = item.Text;
+            }
+
+            var elements1 = chromeDriver.FindElements(By.ClassName("value"));
+
+            return new CompanySearchDto() {
+                Name = companyName,
+                IsRegistered = elements1[0].Text == "(Registered)"? true : false,
+                Details = elements1[1].Text
+            };
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace API
@@ -19,6 +25,7 @@ namespace API
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            IronOcr.Installation.LicenseKey = configuration["IronOcrSettings:LicenseKey"];
         }
 
         public IConfiguration Configuration { get; }
@@ -26,8 +33,21 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IFileService, FileService>();
+            services.AddScoped<ITokenService, TokenService>();
             services.AddControllers();
+            services.AddCors();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                    };
+                });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -44,15 +64,25 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
+            app.UseHsts();
+
+            app.UseHttpsRedirection();
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+            app.UseAuthentication();
+
             app.UseAuthorization();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
